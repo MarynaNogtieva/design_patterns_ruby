f342fa004c884b34e4f16e26cacc6ce4e10ab66b
MarynaNogtieva <marynanogtieva@yahoo.ca>
2020-05-06 23:08:17 -0400

    Add rubocop

--- state/canvas/tool.rb
+++ state/canvas/tool.rb
@@ -1,13 +1,13 @@
 module State
   # State
-  # Abstract class  - want to provide common code 
+  # Abstract class  - want to provide common code
   # to all child classes
   class Tool
     attr_accessor :tool_type
     TOOL_TYPE = {
       selection: 'SELECTION',
-      brush:     'BRUSH'
-      eraser:    'ERAZER'
+      brush: 'BRUSH',
+      eraser: 'ERAZER'
     }.freeze
 
     # Abstract methods
