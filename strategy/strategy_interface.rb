c5b7bce3d6144943678d1ea059efa32e8b8c8c05
MarynaNogtieva <marynanogtieva@yahoo.ca>
2020-05-24 19:21:38 -0400

    Change param name

--- strategy/strategy_interface.rb
+++ strategy/strategy_interface.rb
@@ -7,13 +7,13 @@ module StrategyInterface
   end
 
   module Methods
-    def method_not_implemented(klass)
+    def method_not_implemented(klass_obj)
       # #<MatchData "in `eval'" 1:"eval">
       caller.first.match(/in \`(.+)\'/)
       # https://docs.ruby-lang.org/en/2.4.0/globals_rdoc.html
       # $1 - The Nth group of the last successful match. May be > 1.
       method_name = $1
-      raise IteratorInterface::StrategyInterfaceMethodNotImplemented.new("#{klass.class.name} needs to implement '#{method_name}' for interface #{self.name}!")
+      raise IteratorInterface::StrategyInterfaceMethodNotImplemented.new("#{klass_obj.class.name} needs to implement '#{method_name}' for interface #{self.name}!")
     end
   end
 end
