3582df93cacde205db8ca70d6d72214f55616098
MarynaNogtieva <marynanogtieva@yahoo.ca>
2020-05-26 18:36:30 -0400

    Fix comments

--- template/demo.rb
+++ template/demo.rb
@@ -1,7 +1,9 @@
 require_relative 'service_desk_window'
 # https://refactoring.guru/design-patterns/template-method
-# Template Method is a behavioral design pattern that defines the skeleton of an algorithm in the superclass but lets subclasses override specific steps of the algorithm without changing its structure.
-# The Template Method pattern suggests that you break down an algorithm into a series of steps, turn these steps into methods, and put a series of calls to these methods inside a single “template method.” 
+# Template Method is a behavioral design pattern that defines the skeleton of an algorithm in the superclass
+# but lets subclasses override specific steps of the algorithm without changing its structure.
+# The Template Method pattern suggests that you break down an algorithm into a series of steps,
+# turn these steps into methods, and put a series of calls to these methods inside a single "template method."
 class Demo
   def self.run
     window = ServiceDeskWindow.new
