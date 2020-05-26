e4830ccb49f479e6440c4feb96d5717bdea4f483
MarynaNogtieva <marynanogtieva@yahoo.ca>
2020-05-26 19:08:52 -0400

    Add comment to command demo

--- command/demo.rb
+++ command/demo.rb
@@ -6,7 +6,14 @@ require_relative 'history'
 require_relative 'set_text_command'
 require_relative 'set_contrast_command'
 require_relative 'undo_command'
+# https://refactoring.guru/design-patterns/command
+# Command is a behavioral design pattern that turns a request into a stand-alone object that contains all information about the request.
+#  This transformation lets you parameterize methods with different requests, delay or queue a request’s execution, and support undoable operations.
 
+# In the Exercises project, look at the command/VideoEditor class.
+#  This class represents the operations we can perform on a video,
+#  such as adding a label to it or changing its contrast. 
+# We need to allow the user to undo any changes he/she makes to a video.
 class Demo
   def self.run
     #setup
